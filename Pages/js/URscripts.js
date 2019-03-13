79782f5372e72ff29ae2ba078e21bf2298816fa1
JakeVel <jave4876@colorado.edu>
2019-03-13 15:14:00 -0700

    Registration and loging HTML/JS

---
+++ Pages/js/URscripts.js
@@ -0,0 +1,20 @@
+
+
+function noIntrests(id1,id2,id3,id4)
+{
+	if (document.getElementById(id1).disabled == false)
+	{
+		document.getElementById(id1).disabled = true;
+		document.getElementById(id2).disabled = true;
+		document.getElementById(id3).disabled = true;
+		document.getElementById(id4).disabled = true;
+	}
+
+	else if (document.getElementById(id1).disabled == true)
+	{
+		document.getElementById(id1).disabled = false;
+		document.getElementById(id2).disabled = false;
+		document.getElementById(id3).disabled = false;
+		document.getElementById(id4).disabled = false;
+	}
+}				
