74e913bf1ce23c3bfa348cea978ad3bf00e9195e
JakeVel <jave4876@colorado.edu>
2019-04-03 11:52:55 -0700

    Api Page addition

--- public/js/URscripts.js
+++ public/js/URscripts.js
@@ -17,4 +17,36 @@ function noIntrests(id1,id2,id3,id4)
 		document.getElementById(id3).disabled = false;
 		document.getElementById(id4).disabled = false;
 	}
+}
+
+function openNav1() {
+  document.getElementById("myNav1").style.display = "block";
+}
+
+function openNav2() {
+  document.getElementById("myNav2").style.display = "block";
+}
+
+function openNav3() {
+  document.getElementById("myNav3").style.display = "block";
+}
+
+function openNav4() {
+  document.getElementById("myNav4").style.display = "block";
+}
+
+function closeNav1() {
+  document.getElementById("myNav1").style.display = "none";
+}
+
+function closeNav2() {
+  document.getElementById("myNav2").style.display = "none";
+}
+
+function closeNav3() {
+  document.getElementById("myNav3").style.display = "none";
+}
+
+function closeNav4() {
+  document.getElementById("myNav4").style.display = "none";
 }				
