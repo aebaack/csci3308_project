a9ac6cca84fd5e417685aa47bfffbca628038797
Aidan Baack <aebaack@gmail.com>
2019-04-25 10:01:07 -0600

    Initial mocha and chai setup

---
+++ test/users.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const assert = require('assert');
+const expect = require('chai').expect;
+const should = require('chai').should();
+
+it('First test', () => {
+  expect(true).to.be.true;
+});
